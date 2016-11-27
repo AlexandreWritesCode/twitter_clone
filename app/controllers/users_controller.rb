c955a4f90ca011ef860195c3ad0521f50576f5ce
Alexandre Cabello <cabelloalexandre@gmail.com>
2016-11-27 17:21:42 -0500

    checkup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(@user)
       flash[:success] = "Welcome to Twitter!"
+      redirect_to root_url
     else
       render 'new'
     end
